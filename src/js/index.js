100998e2ef889638612803abd4aa8b615f1cfc64
Roger Zhang <1787009521@qq.com>
2022-02-11 16:27:42 +0800

    feat: replace替换元素

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 (function() {
 
-let str = `<p id="content">书籍《javascript高级程序设计》<span id="count">所剩数量：0</span></p>`
+let str = `<p id="content">书籍《__bookName》<span id="count">所剩数量：__count</span></p>`
+
+str = str.replace('__bookName', 'JavaScript高级程序设计').replace('__count', '0')
 
 $('#box').prepend($(str))
 
