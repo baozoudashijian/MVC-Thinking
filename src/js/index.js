deae618c0e3a92d6cfd2a28d83f0b6071b89e364
Roger Zhang <1787009521@qq.com>
2022-02-11 16:41:28 +0800

    feat: 加减清0

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,26 @@
-(function() {
+(function () {
 
-let str = `<p id="content">书籍《__bookName》<span id="count">所剩数量：__count</span></p>`
+  let data = {
+    count: 0
+  }
+  let str = `<p id="content">书籍《__bookName》<span id="count">所剩数量：__count</span></p>`
 
-str = str.replace('__bookName', 'JavaScript高级程序设计').replace('__count', '0')
+  str = str.replace('__bookName', 'JavaScript高级程序设计').replace('__count', data.count)
 
-$('#box').prepend($(str))
+  $('#box').prepend($(str))
+  $('#addition').bind('click', function () {
+    data.count++
+    $('#count').text(`所剩数量：${data.count}`)
+  })
+
+  $('#subtraction').bind('click', function () {
+    data.count--
+    $('#count').text(`所剩数量：${data.count}`)
+  })
+
+  $('#clear').bind('click', function () {
+    data.count = 0
+    $('#count').text(`所剩数量：${data.count}`)
+  })
 
 })()
