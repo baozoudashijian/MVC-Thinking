947222563bb021e90b1ea22386684a0b775cd1f7
zhangrongjie <1787009521@qq.com>
2022-02-11 20:55:12 +0800

    使用事件委托解决更新dom消失的绑定事件，使用e.target.id解决事件委托导致触发多个事件的问题

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,15 @@
   axios.defaults.baseURL = 'https://null.jsbin.com'
   axios.interceptors.response.use(
     response => {
-      console.log(response)
+      let { method, url } = response.config
+
+      if(method == 'get' && url == '/book/count') {
+        response.data = {
+          name: 'JavaScript高级程序设计',
+          count: 4,
+          id: 1
+        }
+      }
 
       return response
     }
@@ -11,30 +19,36 @@
   let data = {
     count: 0
   }
-  let str = `<p id="content">书籍《__bookName》<span id="count">所剩数量：__count</span></p>`
 
-  str = str.replace('__bookName', 'JavaScript高级程序设计').replace('__count', data.count)
-
-  axios.get('/book/count').then((response) => {
-    console.log(response);
+  axios.get('/book/count').then(({data}) => {
+    let { name, count } = data
+    let originalHtml = $('#app').html()
+    let newHtml = originalHtml.replace('__bookName', name).replace('__count', count)
+    $('#app').html(newHtml)
+    data.count = count
   },(err) => {
     console.log(err)
   })
 
-  $('#app').prepend($(str))
-  $('#addition').bind('click', function () {
-    data.count++
-    $('#count').text(`所剩数量：${data.count}`)
+  $('#app').bind('click', '#addition', function (e) {
+    if(e.target.id == 'addition') {
+      data.count++
+      $('#count').text(`所剩数量：${data.count}`)
+    }
   })
 
-  $('#subtraction').bind('click', function () {
-    data.count--
-    $('#count').text(`所剩数量：${data.count}`)
+  $('#app').bind('click', '#subtraction', function (e) {
+    if(e.target.id == 'subtraction') {
+      data.count--
+      $('#count').text(`所剩数量：${data.count}`)
+    }
   })
 
-  $('#clear').bind('click', function () {
-    data.count = 0
-    $('#count').text(`所剩数量：${data.count}`)
+  $('#app').bind('click', '#clear', function (e) {
+    if(e.target.id == 'clear') {
+      data.count = 0
+      $('#count').text(`所剩数量：${data.count}`)
+    }
   })
 
 })()
