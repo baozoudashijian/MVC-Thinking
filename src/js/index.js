b4fb6667d368bea5cb419c5e361fbd200033200e
zhangrongjie <1787009521@qq.com>
2022-02-11 21:56:58 +0800

    refactor: MVC改造代码

--- src/js/index.js
+++ src/js/index.js
@@ -4,81 +4,106 @@
 *
 */
 (function () {
-  let initialData = {
-    name: 'JavaScript高级程序设计',
-    count: 4,
-    id: 1
-  }
-  axios.defaults.baseURL = 'https://null.jsbin.com'
-  axios.interceptors.response.use(
-    response => {
-      let { method, url } = response.config
 
-      if(method == 'get' && url == '/book/count') {
-        response.data = initialData
-      }
-      if(method == 'get' && url.indexOf('/book/count2') >= 0) {
-        
-        response.data = Object.assign(initialData, {count: getQueryVariable(url)})
-        console.log(Object.assign(initialData, {count: getQueryVariable(url)}))
+  fakeData()
 
-      }
 
-      return response
+  let model = {
+    data: {
+      name: '',
+      count: 0,
+      id: ''
+    },
+    fetch(id) {
+      return axios.get(`/book/${id}`).then((response) => {
+        this.data = response.data
+
+        return response
+      })
+    },
+    update(id,newNumber) {
+      return axios.get(`/book/${id}?number=${newNumber}`).then((response) => {
+        this.data = response.data
+
+        return response
+      })
     }
-  )
-
-  let state = {
-    count: 0
   }
 
-  axios.get('/book/count').then(({data}) => {
+  model.fetch('count').then(({ data }) => {
     let { name, count } = data
     let originalHtml = $('#app').html()
     let newHtml = originalHtml.replace('__bookName', name).replace('__count', count)
     $('#app').html(newHtml)
-    state.count = count
-  },(err) => {
+  }, (err) => {
     console.log(err)
   })
 
   $('#app').bind('click', '#addition', function (e) {
-    if(e.target.id == 'addition') {
-      let newNumber = state.count - 0 + 1
-      axios.get(`/book/count2?number=${newNumber}`).then(({data}) => {
+    if (e.target.id == 'addition') {
+      let newNumber = model.data.count - 0 + 1
+      console.log(newNumber)
+      model.update(`count2`, newNumber).then(({ data }) => {
         let { count } = data
-        state.count = count
-        $('#count').text(`所剩数量：${data.count}`)
+        $('#count').text(`所剩数量：${count}`)
       })
-      
+
     }
   })
 
   $('#app').bind('click', '#subtraction', function (e) {
-    if(e.target.id == 'subtraction') {
-      let newNumber = state.count - 0 - 1
-      axios.get(`/book/count2?number=${newNumber}`).then(({data}) => {
+    if (e.target.id == 'subtraction') {
+      let newNumber = model.data.count - 0 - 1
+      model.update(`count2`, newNumber).then(({ data }) => {
         let { count } = data
-        state.count = count
-        $('#count').text(`所剩数量：${data.count}`)
+        $('#count').text(`所剩数量：${count}`)
       })
     }
   })
 
   $('#app').bind('click', '#clear', function (e) {
-    if(e.target.id == 'clear') {
-      axios.get(`/book/count2?number=0`).then(({data}) => {
+    if (e.target.id == 'clear') {
+      model.update(`count2`, 0).then(({ data }) => {
         let { count } = data
-        state.count = count
-        $('#count').text(`所剩数量：${data.count}`)
+        $('#count').text(`所剩数量：${count}`)
       })
     }
   })
 
-  function getQueryVariable(url){
+
+
+  // Mock数据
+
+  function fakeData() {
+    let initialData = {
+      name: 'JavaScript高级程序设计',
+      count: 4,
+      id: 1
+    }
+    axios.defaults.baseURL = 'https://null.jsbin.com'
+    axios.interceptors.response.use(
+      response => {
+        let { method, url } = response.config
+
+        if (method == 'get' && url == '/book/count') {
+          response.data = initialData
+        }
+        if (method == 'get' && url.indexOf('/book/count2') >= 0) {
+
+          response.data = Object.assign(initialData, { count: getQueryVariable(url) })
+          console.log(Object.assign(initialData, { count: getQueryVariable(url) }))
+
+        }
+
+        return response
+      }
+    )
+  }
+
+  function getQueryVariable(url) {
     let res = url.split("?")[1].split('=')[1];
 
     return res;
-}
+  }
 
 })()
