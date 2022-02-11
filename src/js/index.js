8a7b0f3022607817cccadaa9f4b796b752954b8a
Roger Zhang <1787009521@qq.com>
2022-02-11 17:09:46 +0800

    bug: 模拟数据

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,25 @@
 (function () {
 
+  axios.interceptors.response.use(
+    response => {
+      console.log(response)
+
+      return response
+    }
+  )
+
   let data = {
     count: 0
   }
   let str = `<p id="content">书籍《__bookName》<span id="count">所剩数量：__count</span></p>`
 
   str = str.replace('__bookName', 'JavaScript高级程序设计').replace('__count', data.count)
+
+  axios.get('/book/count').then(({data}) => {
+    console.log(data);
+  },(err) => {
+    console.log(err)
+  })
 
   $('#box').prepend($(str))
   $('#addition').bind('click', function () {
